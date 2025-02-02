a5678d396545e5a4553f45300bf176cead9fff55
farias09 <f.ariasartavia09@gmail.com>
2025-02-02 16:40:44 -0600

    HU 15 - LN, AD y Abstr. Completadas

---
+++ Abstracciones/LN/Interfaces/Ventas/CrearVenta/ICrearVentaLN.cs
@@ -0,0 +1,14 @@
+﻿using Abstracciones.Modelos.Ventas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.LN.Interfaces.Ventas.CrearVenta
+{
+    public interface ICrearVentaLN
+    {
+        Task<int> Crear(VentasDto modelo);
+    }
+}
---
+++ LN/Ventas/CrearVenta/CrearVentaLN.cs
@@ -0,0 +1,34 @@
+﻿using Abstracciones.AD.Interfaces.Ventas.CrearVenta;
+using Abstracciones.LN.Interfaces.General.Conversiones.Ventas.ConvertirAVentasTabla;
+using Abstracciones.LN.Interfaces.Ventas.CrearVenta;
+using Abstracciones.Modelos.Ventas;
+using AcessoADatos.Ventas.CrearVenta;
+using LN.General.Conversiones.Ventas.ConvertirAVentasTabla;
+using LN.Productos.CrearProducto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LN.Ventas.CrearVenta
+{
+    public class CrearVentaLN : ICrearVentaLN
+    {
+        private readonly ICrearVentaAD _crearVentaAD;
+        private readonly IConvertirAVentasTabla _convertirAVentasTabla;
+
+        public CrearVentaLN(ICrearVentaAD crearVentaAD, IConvertirAVentasTabla convertirAVentasTabla)
+        {
+            _crearVentaAD = crearVentaAD;
+            _convertirAVentasTabla = convertirAVentasTabla;
+        }
+
+        public async Task<int> Crear(VentasDto modelo)
+        {
+            modelo.estado = true;
+            int cantidadDeDatosGuardados = await _crearVentaAD.Crear(_convertirAVentasTabla.ConvertirObjetoAVentasTabla(modelo));
+            return cantidadDeDatosGuardados;
+        }
+    }
+}
