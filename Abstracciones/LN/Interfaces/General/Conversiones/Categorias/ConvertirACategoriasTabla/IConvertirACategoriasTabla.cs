d1b6f6366bfd388b49bf34b7ea615488c577621f
matiasorellana <matias.orellana.campos@gmail.com>
2025-02-03 17:21:29 -0600

    HU5 Completa

---
+++ Abstracciones/LN/Interfaces/General/Conversiones/Categorias/ConvertirACategoriasTabla/IConvertirACategoriasTabla.cs
@@ -0,0 +1,15 @@
+﻿using Abstracciones.Modelos.Categorias;
+using Abstracciones.ModelosBaseDeDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.LN.Interfaces.General.Conversiones.Categorias.ConvertirACategoriasTabla
+{
+    public interface IConvertirACategoriasTabla
+    {
+        CategoriasTabla ConvertirObjetoACategoriasTabla(CategoriasDto laCategoria);
+    }
+}
---
+++ LN/Categorias/ListarCategorias/ListarCategoriasLN.cs
@@ -0,0 +1,39 @@
+﻿using Abstracciones.AD.Interfaces.Categorias.ListarCategorias;
+using Abstracciones.LN.Interfaces.Categorias.ListarCategorias;
+using Abstracciones.LN.Interfaces.General.Conversiones.Categorias.ConvertirACategoriasDto;
+using Abstracciones.Modelos.Categorias;
+using Abstracciones.ModelosBaseDeDatos;
+using AcessoADatos.Categorias.ListarCategorias;
+using LN.General.Conversiones.Categorias.ConvertirACategoriasDto;
+using System.Collections.Generic;
+
+namespace LN.Categorias.ListarCategorias
+{
+    public class ListarCategoriasLN : IListarCategoriasLN
+    {
+        IListarCategoriasAD _listarCategoriasAD;
+        IConvertirACategoriasDto _convertirACategoriasDto;
+
+        public ListarCategoriasLN()
+        {
+            _listarCategoriasAD = new ListarCategoriasAD();
+            _convertirACategoriasDto = new ConvertirACategoriasDto();
+        }
+
+        public List<CategoriasDto> Listar()
+        {
+            List<CategoriasDto> laListaDeCategorias = _listarCategoriasAD.Listar();
+            return laListaDeCategorias;
+        }
+
+        private List<CategoriasDto> ObtenerLaListaConvertida(List<CategoriasTabla> laListaDeCategorias)
+        {
+            List<CategoriasDto> laListaDeCodigos = new List<CategoriasDto>();
+            foreach (CategoriasTabla laCategoria in laListaDeCategorias)
+            {
+                laListaDeCodigos.Add(_convertirACategoriasDto.ConvertirObjetoACategoriasDto(laCategoria));
+            }
+            return laListaDeCodigos;
+        }
+    }
+}
